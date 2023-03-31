7c46d264d38cd053161238956655b75cb49b6a98
BAhern98 <brendan@ballysheen.net>
2023-03-31 05:53:13 +0200

    Spring boot project created

---
+++ RestAPI-MetaData/src/main/java/net/codejava/song/RestApiMetaDataApplication.java
@@ -0,0 +1,13 @@
+package net.codejava.song;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiMetaDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiMetaDataApplication.class, args);
+	}
+
+}
---
+++ RestAPI-MetaData/src/test/java/net/codejava/song/RestApiMetaDataApplicationTests.java
@@ -0,0 +1,13 @@
+package net.codejava.song;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiMetaDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
