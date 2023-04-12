c045960db94db065fba48dce2e15f5d82fd7262b
BAhern98 <brendan@ballysheen.net>
2023-04-13 00:36:39 +0200

    containerized database, removed dockerfile, added docker-compose

--- RestAPI-MetaData/src/main/resources/schema.sql
+++ RestAPI-MetaData/src/main/resources/schema.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `track`
 --
 
+
+
 DROP TABLE IF EXISTS `track`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
